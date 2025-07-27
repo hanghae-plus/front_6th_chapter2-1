7f8b6cd7b737f20dea93c1a24381de0bf8ac0497
changjun <ckdwns9121@naver.com>
2025-07-28 01:28:02 +0900

    refactor: 매직넘버 상수화

---
+++ src/basic/constants/POINTS.js
@@ -0,0 +1,14 @@
+// 포인트 관련 상수들
+export const POINTS = {
+  BASE_RATE: 1000, // 기본 포인트 적립 기준 (구매액 / 1000)
+  TUESDAY_MULTIPLIER: 2, // 화요일 포인트 2배
+  KEYBOARD_MOUSE_SET: 50, // 키보드+마우스 세트 추가 포인트
+  FULL_SET: 100, // 풀세트 구매 추가 포인트
+};
+
+// 수량별 추가 포인트
+export const QUANTITY_BONUS_POINTS = {
+  SMALL_BULK: 20, // 10개 이상
+  MEDIUM_BULK: 50, // 20개 이상
+  LARGE_BULK: 100, // 30개 이상
+};
---
+++ src/basic/constants/index.js
@@ -0,0 +1,6 @@
+// 모든 상수들을 한 곳에서 export
+export * from "./PRODUCT_IDS.js";
+export * from "./PRICES.js";
+export * from "./QUANTITIES.js";
+export * from "./POINTS.js";
+export * from "./TIMERS.js";
