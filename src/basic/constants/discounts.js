4e9b61537ee4e6f6849e910238ce5298b581b8a3
eveneul <nebulas9724@gmail.com>
2025-07-29 01:36:02 +0900

    할인 규칙 추가 및 주석 개선
    - discounts.js: 개별 상품 할인 외에 대량 주문, 화요일 추가 할인, 번개 세일, 추천 할인에 대한 규칙 추가
    - 코드 가독성을 위해 주석을 추가하여 할인 내용 설명

--- src/basic/constants/discounts.js
+++ src/basic/constants/discounts.js
@@ -1,9 +1,27 @@
-import { PRODUCT_IDS } from "./products";
+import { PRODUCT_IDS } from './products';
 
+/**
+ * 개별 상품 할인
+ * 모두 퍼센트 단위 (0.1 === 10%)
+ */
 export const DISCOUNT_RATES = {
   [PRODUCT_IDS.KEYBOARD]: 0.1,
   [PRODUCT_IDS.MOUSE]: 0.15,
   [PRODUCT_IDS.MONITOR_ARM]: 0.2,
   [PRODUCT_IDS.LAPTOP_POUCH]: 0.05,
   [PRODUCT_IDS.SPEAKER]: 0.25,
+};
+
+/**
+ * 30개 이상 주문 시 25퍼센트 할인
+ * 화요일 10퍼센트 할인 추가
+ * 번개 세일 20퍼센트 할인
+ * 추천 할인 5퍼센트 할인
+ */
+
+export const DISCOUNT_RULES = {
+  BULK_DISCOUNT_RATE: 0.25,
+  TUESDAY_DISCOUNT_RATE: 0.1,
+  LIGHTNING_SALE_RATE: 0.2,
+  SUGGEST_SALE_RATE: 0.05,
 };
