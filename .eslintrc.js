aa29cb366876983f4004c1b60ef131498f8f4d68
최용훈 <yonghun@douzone.com>
2025-07-28 22:12:22 +0900

    Chore: eslint & prettier 설정.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    // 여기에 custom 규칙 추가
+    'prettier/prettier': 'error',
+  },
+};
