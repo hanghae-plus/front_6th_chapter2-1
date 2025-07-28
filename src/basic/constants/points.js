beca3ec0076ee1dc196f167f277ca7519db041de
eveneul <nebulas9724@gmail.com>
2025-07-29 01:33:13 +0900

    포인트 관리 상수 추가
    - points.js: 포인트 관련 상수 정의 및 기본 구매액, 화요일 배수, 다양한 보너스 설정 추가

---
+++ src/basic/constants/points.js
@@ -0,0 +1,16 @@
+/**
+ * 포인트들을 관리하기 위해 상수로 재정의
+ * 기본 구매액 0.1 퍼센트
+ * 화요일 기본 포인트의 2배
+ * 그외는 포인트 단위 적립
+ */
+
+export const POINTS = {
+  BASE_RATE: 0.01,
+  TUESDAY_MULTIPLIER: 2,
+  KEYBOARD_MOUSE_COMBO: 50,
+  FULL_SET: 100,
+  BULK_10_BONUS: 20,
+  BULK_20_BONUS: 50,
+  BULK_30_BONUS: 100,
+};
