2894449c26ac6a4876d75c9e3dbb3c2c01ef6ea3
JangRuBin2 <wkdfnqls2465@gmail.com>
2025-07-30 22:03:19 +0900

    feat: 컴포너트만 분리

---
+++ src/components/ManualOverlay.js
@@ -0,0 +1,5 @@
+export const ManualOverlay = () => {
+  const manualOverlay = document.createElement("div");
+  manualOverlay.className = "fixed inset-0 bg-black/50 z-10";
+  return manualOverlay;
+};
---
+++ src/components/SelectContainer.js
@@ -0,0 +1,5 @@
+export const SelectContainer = () => {
+  const selectContainer = document.createElement("div");
+  selectContainer.className = "mb-6 pb-6 border-b border-gray-200";
+  return selectContainer;
+};
