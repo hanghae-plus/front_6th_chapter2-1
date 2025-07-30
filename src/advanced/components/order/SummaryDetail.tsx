364f9fa2dcd30997794c57fe6a82292eea71e9e4
Angie Lee <angielxx94@gmail.com>
2025-07-31 02:03:30 +0900

    feat: 주문 목록 렌더링

--- src/advanced/components/order/SummaryDetail.tsx
+++ src/advanced/components/order/SummaryDetail.tsx
@@ -1,17 +1,35 @@
-import { ReactElement } from 'react';
-
 import Divider from '@/advanced/components/layout/Divider';
 import useOrderSummary from '@/advanced/hooks/useOrderSummary';
+import { useCartStore } from '@/advanced/store';
 import formatPrice from '@/advanced/utils/format.util';
 
-export default function SummaryDetail(): ReactElement {
+export default function SummaryDetail() {
+  const { cartItems } = useCartStore();
+
   const { subTotal } = useOrderSummary();
+
+  const orderList = cartItems.map(cartItem => {
+    const { name, price, quantity } = cartItem;
+
+    const totalPrice = price * quantity;
+    const formattedTotalPrice = formatPrice(totalPrice);
+
+    return (
+      <div key={cartItem.id} className="flex justify-between text-xs tracking-wide text-gray-400">
+        <span>
+          {name} x {quantity}
+        </span>
+        <span>{formattedTotalPrice}</span>
+      </div>
+    );
+  });
 
   const formattedSubTotal = formatPrice(subTotal);
 
   return (
     <div id="summary-details" className="space-y-3">
       {/* 주문 요약 내용 */}
+      {orderList}
 
       <Divider />
 
--- src/advanced/hooks/useOrderSummary.ts
+++ src/advanced/hooks/useOrderSummary.ts
@@ -1,9 +1,9 @@
+import { useCartStore } from '@/advanced/store';
+
 export default function useOrderSummary() {
-  // const { cartItems } = useCartStore();
+  const { cartItems } = useCartStore();
 
-  // const subTotal = cartItems.reduce((acc, item) => acc + item.price, 0);
-
-  const subTotal = 0;
+  const subTotal = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
   return { subTotal };
 }
