370fbbad3fbbedd86f746c08ca1f2912362d9ecf
ure <uy23@naver.com>
2025-07-30 16:23:41 +0900

    feat: Manual 모달 추가

--- src/advanced/src/App.tsx
+++ src/advanced/src/App.tsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './index.css';
 import Header from './components/Header';
 import MainLayout from './components/MainLayout';
 import CartDisplay from './components/CartDisplay';
 import OrderSummary from './components/OrderSummary';
+import InfoButton from './components/ManualButton';
+import ManualModal from './components/ManualModal';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
     <div className="bg-gray-50 text-black overflow-hidden text-sm max-w-screen-xl h-screen max-h-800 mx-auto p-8 flex flex-col">
       <Header count={0} />
       <MainLayout left={<CartDisplay />} right={<OrderSummary />} />
+      <InfoButton onClick={handleOpenModal} />
+      <ManualModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 }
