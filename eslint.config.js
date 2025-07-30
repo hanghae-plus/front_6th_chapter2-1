c21b41dbf266c5f53cebaedc674c6fec675eae86
tksemf7410 <tksemf7410@gmail.com>
2025-07-31 00:29:54 +0900

    chore: advanced 에 react 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,40 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import {
+  defineConfig
+} from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['src/advanced/**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginReact.configs.flat.recommended,
 ]);
