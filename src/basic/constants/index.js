ea5bda55b810702b0ff2f0f599e9d9ee4f3be12f
ChangJun Park <40492343+devchangjun@users.noreply.github.com>
2025-07-30 16:47:24 +0900

    refactor: 불필요한 매직넘버 삭제제

--- src/basic/constants/index.js
+++ src/basic/constants/index.js
@@ -1,7 +1,5 @@
-// 모든 상수들을 한 곳에서 export
-export * from "./product.js";
 export * from "./quantity.js";
 export * from "./point.js";
 export * from "./timer.js";
 export * from "./price.js";
-export * from "./discount.js";
+export * from "./product.js";
