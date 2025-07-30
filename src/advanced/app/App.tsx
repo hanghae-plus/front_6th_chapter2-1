4f766bbdce7cf1ab8e7f15e5bd26b977eb0c611f
Dan <geonhwi.jung@specter.co.kr>
2025-07-31 01:14:27 +0900

    fix: design 통일

--- src/advanced/app/App.tsx
+++ src/advanced/app/App.tsx
@@ -15,30 +15,23 @@ const AppContent: React.FC = () => {
   const { products, updateProducts } = useCart();
 
   return (
-    <PromotionProvider 
-      products={products} 
-      onProductsUpdate={updateProducts}
-    >
-      <div className="min-h-screen bg-gray-50 p-6">
-        <div className="container mx-auto max-w-7xl">
-          <Header />
-          
-          {/* 메인 그리드 - 기본과제와 동일한 레이아웃 */}
-          <div className="grid grid-cols-1 lg:grid-cols-[1fr_360px] gap-6 flex-1 overflow-hidden">
-            {/* 왼쪽 컬럼 */}
-            <div className="bg-white border border-gray-200 p-8 overflow-y-auto">
-              <ProductSelector />
-              <CartList />
-            </div>
-            
-            {/* 오른쪽 컬럼 */}
-            <OrderSummary />
-          </div>
+    <PromotionProvider products={products} onProductsUpdate={updateProducts}>
+      <Header />
+
+      {/* 메인 그리드 - 기본과제와 동일한 레이아웃 */}
+      <div className='grid grid-cols-1 lg:grid-cols-[1fr_360px] gap-6 flex-1 overflow-hidden'>
+        {/* 왼쪽 컬럼 */}
+        <div className='bg-white border border-gray-200 p-8 overflow-y-auto'>
+          <ProductSelector />
+          <CartList />
         </div>
-        
-        {/* 도움말 모달 */}
-        <HelpModal />
+
+        {/* 오른쪽 컬럼 */}
+        <OrderSummary />
       </div>
+
+      {/* 도움말 모달 */}
+      <HelpModal />
     </PromotionProvider>
   );
 };
