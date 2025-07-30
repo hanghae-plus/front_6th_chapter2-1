667b35513173d28cd9b0da7e599cfe3d7e25d7be
eveneul <nebulas9724@gmail.com>
2025-07-30 23:47:36 +0900

    상품 옵션 스타일링 상수 및 할인 아이콘 조합 추가

---
+++ src/basic/constants/styles.js
@@ -0,0 +1,15 @@
+// 상품 옵션 스타일링 상수
+export const PRODUCT_OPTION_STYLES = {
+  OUT_OF_STOCK: 'text-gray-400',
+  SUPER_SALE: 'text-purple-600 font-bold',
+  LIGHTNING_SALE: 'text-red-500 font-bold',
+  SUGGESTION_SALE: 'text-blue-500 font-bold',
+  LOW_STOCK_BORDER: 'orange',
+};
+
+// 상품 할인 아이콘 조합
+export const SALE_ICONS = {
+  LIGHTNING: '⚡',
+  SUGGESTION: '💝',
+  SUPER_COMBO: '⚡💝',
+};
