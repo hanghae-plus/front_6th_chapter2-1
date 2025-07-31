6a241ed4242a1a02d8a89fcde62e094ac76cf028
chan9yu <dev.cgyeo@gmail.com>
2025-07-31 15:08:25 +0900

    chore: 배포를 위한 설정 추가

--- apps/advanced/vite.config.ts
+++ apps/advanced/vite.config.ts
@@ -1,7 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
+const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter2-1/" : "";
+
 export default defineConfig({
+	base,
 	plugins: [react()],
 	test: {
 		globals: true,
