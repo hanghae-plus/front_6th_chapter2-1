92b9f742f5d061233036b3877865e8c1fb65b426
tksemf7410 <tksemf7410@gmail.com>
2025-07-31 23:20:05 +0900

    feat: getStockInfoMessage 추가

--- src/advanced/components/selector/StockInfoText.jsx
+++ src/advanced/components/selector/StockInfoText.jsx
@@ -1,4 +1,17 @@
-import { getStockInfoMessage } from "../../../basic/entity/stock";
+import { prodList } from "../../data";
+
+export const getStockInfoMessage = () => {
+  return prodList.reduce((acc, item) => {
+    if (item.quantity < 5) {
+      if (item.quantity > 0) {
+        acc += `${item.name}: 재고 부족 (${item.quantity}개 남음)\n`;
+      } else {
+        acc += `${item.name}: 품절\n`;
+      }
+    }
+    return acc;
+  }, "");
+};
 
 export const StockInfoText = () => {
   return (
