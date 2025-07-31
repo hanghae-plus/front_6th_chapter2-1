2b54ae229d69bf8012ca8fe26753db4874f4b83d
BangDori <qudwns8865@naver.com>
2025-08-01 04:09:04 +0900

    chore: vite 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/front_6th_chapter2-1/',
   build: {
     rollupOptions: {
-      input: {
-        main: 'index.html',
-        basic: 'index.basic.html',
-        advanced: 'index.advanced.html',
-      },
+      input: 'index.advanced.html',
     },
   },
   test: {
