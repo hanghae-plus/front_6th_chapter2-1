ce08693cabfa84724bb28f1eb6ea11b377cb7eb5
YeongseoYoon <l112xpla@gmail.com>
2025-08-01 06:04:26 +0900

    feat: 패키지 및 빌드 설정 업데이트
    - package.json에서 코드 품질 검사 명령어를 pnpm으로 변경하고, gh-pages 배포 스크립트를 추가하였습니다.
    - vite.config.js에서 빌드 설정을 추가하여 여러 HTML 파일을 입력으로 지정하였습니다.
    - pnpm-lock.yaml 파일을 업데이트하여 새로운 의존성을 반영하였습니다.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vitest/config';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/front_6th_chapter2-1/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        advanced: resolve(__dirname, 'index.advanced.html'),
+        basic: resolve(__dirname, 'index.basic.html'),
+      },
     },
   },
   test: {
