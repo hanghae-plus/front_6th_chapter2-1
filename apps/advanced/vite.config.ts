fce80e175b7f3484c6bfc8e86e5ed194e155d6fb
BBAK_JUN <wnsguddl789@gmail.com>
2025-07-31 15:43:47 +0900

    feat: gh-pages 패키지 추가 및 Vite 설정 파일 생성 - 배포를 위한 빌드 및 설정 구성

--- apps/advanced/vite.config.ts
+++ apps/advanced/vite.config.ts
@@ -1,7 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const base = process.env.NODE_ENV === 'production' ? "/front_6th_chapter2-1/" : '';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   resolve: {
     alias: {
