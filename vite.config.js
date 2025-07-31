cbba073f352237ca48649e99bc98413102a62b9a
bebusl <bebus1998@naver.com>
2025-07-31 09:10:50 +0900

    fix: 배포 BASE경로 수정 및 gh-pages 스크립트에 build 명령어 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 
+const base = process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-1/' : '';
+
 export default defineConfig({
+  base,
   test: {
     globals: true,
     environment: 'jsdom',
@@ -10,8 +13,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        basic: path.resolve('index.basic.html'),
-        advanced: path.resolve('index.advanced.html'),
+        basic: path.resolve(__dirname, 'index.basic.html'),
+        advanced: path.resolve(__dirname, 'index.advanced.html'),
       },
     },
   },
