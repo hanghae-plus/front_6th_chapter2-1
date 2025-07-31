71cd7d9a443c54cbb6ed637815584f8deb1434e8
esoby <esoby104@gmail.com>
2025-08-01 01:30:31 +0900

    chore: 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/front_6th_chapter2-1/',
+  build: {
+    rollupOptions: {
+      input: 'index.advanced.html',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/setupTests.js',
   },
+  plugins: [react()],
 });
