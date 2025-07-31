9eb590e83ff4ae6f20892284b46c06e72447ed18
BangDori <qudwns8865@naver.com>
2025-08-01 01:47:30 +0900

    feat: 심화과제 스타트!!!!

---
+++ src/advanced/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => (
+  <div className='min-h-screen bg-red-500 p-32'>
+    <div className='max-w-4xl mx-auto'>
+      <h1 className='text-3xl font-bold text-gray-900 mb-4'>Shopping Cart - Advanced</h1>
+      <p className='text-gray-600'>Tailwind CSS v4.0이 설치되었습니다!</p>
+    </div>
+  </div>
+);
+
+export default App;
