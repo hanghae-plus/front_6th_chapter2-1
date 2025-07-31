0c0cb0d1c264dd1fb9cc1ab1abd89d135ae3b08d
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-31 13:27:27 +0900

    deploy : 배포설정

--- src/advanced/vite.config.ts
+++ src/advanced/vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/front_6th_chapter2-1/",
   test: {
     globals: true,
     environment: "jsdom",
