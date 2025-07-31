c41d0da333950cc51a25b70e84f4cfd90627c199
tksemf7410 <tksemf7410@gmail.com>
2025-08-01 07:45:41 +0900

    feat: 빌드 설정 추가

---
+++ vite.advanced.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
+  },
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./index.advanced.html",
+      },
+    },
+  },
+});
