224e5e531429b3ca7fab921d4fbf7772fe78c7ca
tksemf7410 <tksemf7410@gmail.com>
2025-08-01 07:51:52 +0900

    feat: 배포 base 경로 추가

--- vite.advanced.config.js
+++ vite.advanced.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
+const base =
+  process.env.NODE_ENV === "production" ? "/front_6th_chapter2-1/" : "";
+
 export default defineConfig({
+  base,
   test: {
     globals: true,
     environment: "jsdom",
