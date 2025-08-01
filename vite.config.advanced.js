9373223c92b1173663bd40ff376335a6fbd43629
Legitgoons <cksxkr5193@naver.com>
2025-08-01 21:43:03 +0900

    fix: 배포 경로 수정

--- vite.config.advanced.js
+++ vite.config.advanced.js
@@ -3,15 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "src/advanced",
   base: "/front_6th_chapter2-1/",
   build: {
-    outDir: "../../dist",
+    rollupOptions: {
+      input: "./index.advanced.html",
+    },
+    outDir: "dist",
     emptyOutDir: true,
   },
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "../setupTests.js",
+    setupFiles: "src/setupTests.js",
   },
 });
