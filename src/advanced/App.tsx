ba6aedd1ba43d4410dc0df7e9861fea2eb6f71b4
youngmin <jym0421@ciel.co.kr>
2025-08-01 15:31:00 +0900

    feat: 상품셀렉터 추가

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,4 +1,5 @@
 import Header from '@advanced/components/Header';
+import ProductSelector from '@advanced/components/ProductSelector';
 
 const App = () => {
   return (
@@ -10,10 +11,7 @@ const App = () => {
         <div className="bg-white border border-gray-200 p-8 overflow-y-auto">
           {/* selectorContainer */}
           <div className="mb-6 pb-6 border-b border-gray-200">
-            <select
-              className="w-full p-3 border border-gray-300 rounded-lg text-base mb-3"
-              id="product-select"
-            ></select>
+            <ProductSelector />
             <button
               className="w-full py-3 bg-black text-white text-sm font-medium uppercase tracking-wider hover:bg-gray-800 transition-all"
               id="add-to-cart"
