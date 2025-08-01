3826c7905210311d56f3923915d0ddbe563e9fc6
JOYOUNGMIN <jym0421@ciel.co.kr>
2025-08-02 00:22:23 +0900

    feat: 장바구니 상품 추가 함수 추가

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,15 +1,66 @@
 import Header from '@advanced/components/Header';
 import ProductSelector from '@advanced/components/ProductSelector';
 import { productList } from '@advanced/feature/product/constant';
+import { type Product } from '@advanced/feature/product/type';
 import { getStockInfo } from '@basic/features/product/service';
 import { type ChangeEvent, useState } from 'react';
 
 const App = () => {
-  const [products, setProducts] = useState(productList);
-  const [selectedProduct, setSelectedProduct] = useState<string>('');
+  const [products, setProducts] = useState<Product[]>(productList);
+  const [selectedProductId, setSelectedProductId] = useState<string>(products[0]?.id ?? '');
+  const [cart, setCart] = useState<Product[]>([]);
 
   const handleSelectProduct = (e: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedProduct(e.target.value);
+    setSelectedProductId(e.target.value);
+  };
+
+  const handleAddToCart = () => {
+    const cartProduct = cart.find((cartProduct) => cartProduct.id === selectedProductId);
+    const selectedProduct = products.find((product) => product.id === selectedProductId);
+
+    if (!selectedProduct) {
+      return;
+    }
+
+    if (selectedProduct.quantity === 0) {
+      return;
+    }
+
+    if (!cartProduct) {
+      setCart((prevCart) => [...prevCart, { ...selectedProduct, quantity: 1 }]);
+
+      setProducts((prevProducts) => {
+        return prevProducts.map((product) => {
+          if (product.id === selectedProductId) {
+            return { ...product, quantity: product.quantity - 1 };
+          }
+
+          return product;
+        });
+      });
+
+      return;
+    }
+
+    setCart((prevCart) =>
+      prevCart.map((product) => {
+        if (product.id === selectedProductId) {
+          return { ...product, quantity: product.quantity + 1 };
+        }
+
+        return product;
+      })
+    );
+
+    setProducts((prevProducts) =>
+      prevProducts.map((product) => {
+        if (product.id === selectedProductId) {
+          return { ...product, quantity: product.quantity - 1 };
+        }
+
+        return product;
+      })
+    );
   };
 
   return (
@@ -21,10 +72,11 @@ const App = () => {
         <div className="bg-white border border-gray-200 p-8 overflow-y-auto">
           {/* selectorContainer */}
           <div className="mb-6 pb-6 border-b border-gray-200">
-            <ProductSelector products={products} value={selectedProduct} onChange={handleSelectProduct} />
+            <ProductSelector products={products} value={selectedProductId} onChange={handleSelectProduct} />
             <button
               className="w-full py-3 bg-black text-white text-sm font-medium uppercase tracking-wider hover:bg-gray-800 transition-all"
               id="add-to-cart"
+              onClick={handleAddToCart}
             >
               Add to Cart
             </button>
@@ -33,7 +85,11 @@ const App = () => {
             </div>
           </div>
           {/* cartContainerEl */}
-          <div id="cart-items"></div>
+          <div id="cart-items">
+            {cart.map((cartProduct) => (
+              <div>{`${cartProduct.name}: ${cartProduct.quantity}`}</div>
+            ))}
+          </div>
         </div>
         {/* rightColumn */}
         <div className="bg-black text-white p-8 flex flex-col"></div>
