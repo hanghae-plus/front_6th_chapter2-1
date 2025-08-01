0a3209e3344164c21e22924670e119720e359acf
youngmin <jym0421@ciel.co.kr>
2025-08-01 15:04:46 +0900

    chore: 경로 설정 js 추가

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
       '@basic': path.resolve(__dirname, './src/basic'),
       '@advanced': path.resolve(__dirname, './src/advanced'),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
 });
